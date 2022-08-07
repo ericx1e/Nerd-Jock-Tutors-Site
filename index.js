edc360994b15391368b2fafacaf0ff89cfbc880c
Eric Xie <ericxie6@gmail.com>
2022-08-07 02:07:54 -0400

    lots of stuff

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ window.onscroll = function (e) {
     } else {
         document.getElementById("navbar").className = "navbar_shadow"
     }
+
+    // document.getElementById("navbar").style.backgroundColor = rgb(window.scrollY, 255, 255)
     // console.log(window.scrollY)
 }
